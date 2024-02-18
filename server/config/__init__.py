61d45d63ecf46756b4fac8ca03d224b68fb2efbb
kre8mymedia <kre8mymedia@gmail.com>
2024-02-18 00:44:13 -0600

    Will show server on preview and production

--- server/config/__init__.py
+++ server/config/__init__.py
@@ -5,4 +5,4 @@ VERCEL_ENV = os.environ.get("VERCEL_ENV", 'development')
 VERCEL_URL = os.environ.get("VERCEL_URL", 'localhost:8001')
 
 # Dependents
-PROTOCOL = 'https' if VERCEL_ENV == 'production' else 'http'
+PROTOCOL = 'https' if VERCEL_ENV in {'production', 'preview'} else 'http'
